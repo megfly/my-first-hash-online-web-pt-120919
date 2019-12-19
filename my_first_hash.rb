14900d24d17b31b9c5189fcc614661138c74edb8
Meghan Flynn <mflynn333@gmail.com>
2019-12-19 01:37:07 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -35,12 +35,12 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
 
-
+"muskets" => 2
 
   # add 4 gun powder to the shipping_manifest hash below
 
-
+"gun powder" => 4
 
   # return the shipping_manifest hash below
-
+shipping_manifest
 end
